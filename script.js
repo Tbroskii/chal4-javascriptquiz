1bf005765bc88f25e80081cb26c2c249e1f7a338
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-07-11 11:01:09 -0500

    completeted website

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ var studentAnswer;
 var quiz = document.getElementById("quiz");
 var time = document.getElementById("current-time");
 var response = document.getElementById("show-response");
+var clearScores = document.getElementById("clear-scores");
+var highScoreDisplay = document.getElementById("show-highscores");
 var secondsLeft = 75;
 var index = 0;
 var playerScore = 0;
-var scoreList = [];
 var playerInitials = "";
 
-localStorage.setItem("allPlayerScore", localStorage.getItem("allPlayerScore") + ("playerScore"));
-
-console.log(localStorage.getItem("playerScore"));
-
+//Setting an array of questions to be iterated through
 var questions = 
 [
 '<h1 id="question">Which of the following methods can be used to add an element to the end of an array?</h1><ul id="answers"><li><button onclick="correctAnswer()" > a) push()</button></li><li><button onclick="incorrectAnswer()" > b) unshift()</button></li><li><button onclick="incorrectAnswer()" > c) concat()</button></li><li><button onclick="incorrectAnswer()" > d) pop()</button></li></ul>',
@@ -45,23 +43,30 @@ function setTime () {
 //starting timer
 setTime();
 }
+//Displaying highscores on view high scores page
+highScoreDisplay.innerHTML = localStorage.getItem("allPlayerScores");
 
-
-
+//handling when correct answer is clicked
 function correctAnswer (){
+    //iterate through the array
     index++;
 
+    //clear old question and insert new one
     quiz.innerHTML = "";
     quiz.innerHTML += questions[index];
     
+    //show if last question answered was correct of incorrect
     response.innerHTML = "";
     response.innerHTML = '<h2>Correct</h2>';
 
+    //seeing when all questions have been asked
     if (index === questions.length)
     {
         endQuiz();
     }
 }
+
+//handling when incorrect answer is clicked
 function incorrectAnswer () {
     index++;
 
@@ -79,17 +84,45 @@ function incorrectAnswer () {
 }
 
 function endQuiz() {
-
+    // getting player score
     playerScore = secondsLeft;
+    //clearing quiz after it has been completing
     quiz.innerHTML = "";
+    //inserting intial entry html
     quiz.innerHTML = '<h1 id="question">All done!</h1><p id="score">Your final score is </p><form id="playerScoreForm" action="highscore.html"><span id="score"> Enter intials: </span><input type="text" name="" id="playerScore"><button id="submitScore">Submit</button></form>';
     var playerScoreTxt = document.getElementById("score");
     playerScoreTxt.textContent += playerScore + ".";
     var txtBx = document.getElementById("playerScore");
     var submitInitials = document.getElementById("submitScore");
+
     submitInitials.onclick = function(){
-        localStorage.setItem("playerScore", txtBx.value + " - " + playerScore.toString() + ",");
+        //making sure textbox has an entry if not will auto fill with UNKNOWn
+        if (!(txtBx.value === ""))
+        {
+            localStorage.setItem("playerScore","<li>" + txtBx.value + " - " + playerScore.toString() + "</li>");
+        }
+        else
+        {
+            localStorage.setItem("playerScore","<li>" + "Unknown" + " - " + playerScore.toString() + "</li>");
+        }
+
+        //adding created li to list that will be displayed
+        if (localStorage.getItem("allPlayerScores") === null)
+        {
+            localStorage.setItem("allPlayerScores", localStorage.getItem("playerScore"));
+        }
+        else
+        {
+            localStorage.setItem("allPlayerScores", localStorage.getItem("allPlayerScores") +  localStorage.getItem("playerScore"));
+        }
     }
-    
+}
+
+
+clearScores.onclick = function() {
+    //clearing local storage
+    localStorage.clear();
+    //clearing all displayed 
+    highScoreDisplay.innerHTML = "";
 }
 
